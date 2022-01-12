95d1f666b4e6da5584779163cdbfbe4b1b1563a4
Harjasleen Gill <hgill3@eagles.ewu.edu>
2022-01-11 16:34:28 -0800

    "made some changes"

---
+++ Lecture1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class program
+{
+	public void main()
+	{
+		string text = "Inigo Montoya";
+		Guid guid = Guid.NewGuid();
+
+		Console.WriteLine("Hello, World!");
+		Console.WriteLine("Inigo Montoya" + text);
+	}
+
+}
+
